8741014671a1952ce76965e64a4e86ca238b84b6
Boris_il_forte <boris.ilpossente@hotmail.it>
2016-11-23 23:37:14 +0100

    Started implementation of PixyDemo
    - added PixyPublisher
    - added PixyDemo
    - modified PixyTeleop
    - updated CMakeLists.txt

--- triskar/src/PixyTeleop.cpp
+++ triskar/src/PixyTeleop.cpp
@@ -1,33 +1,44 @@
+/*
+ * triskar,
+ *
+ *
+ * Copyright (C) 2015 Davide Tateo & Matteo Pirotta
+ * Versione 1.0
+ *
+ * This file is part of triskar.
+ *
+ * triskar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * triskar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with triskar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
-#include <triskar_msgs/PixyServo.h>
+
+#include "triskar/PixyPublisher.h"
 
 
-class PixyTeleopJoy
+class PixyTeleopJoy : public PixyPublisher
 {
 public:
 	PixyTeleopJoy(ros::NodeHandle& nh) 
-		: nh(nh), rate(0)
+		: PixyPublisher(nh)
 	{
 		nh.param("reset_button", resetButton, -1);
 		nh.param("axis_pan", panAxis, 1);
 		nh.param("axis_tilt", tiltAxis, 2);
 		nh.param("scale", scale, 1.0);
 
-		double freq;
-		nh.param("rate", freq, 1.0);
-		rate = ros::Rate(freq);
-
-		nh.param("minPan", minPan, 0);
-		nh.param("minTilt", minTilt, 0);
-		nh.param("maxPan", maxPan, 2000);
-		nh.param("maxTilt", maxTilt, 2000);
-
-		pan = (maxPan - minPan) / 2.0;
-		tilt = (maxTilt - minTilt) / 2.0;
-
 		joy_sub = nh.subscribe<sensor_msgs::Joy>("/joy", 5, &PixyTeleopJoy::joyCallback, this);
-		cmd_pub = nh.advertise<triskar_msgs::PixyServo>("/pixy_servo", 5);
 	}
 	
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
@@ -46,44 +57,13 @@ public:
 			tilt =  bound(tilt + tiltCommand, minTilt, maxTilt);
 		}
 	}
-	
-	void spin()
-	{
-		triskar_msgs::PixyServo cmd_msg;
-		cmd_msg.pan = std::ceil(pan);
-		cmd_msg.tilt = std::ceil(tilt);
-		cmd_pub.publish(cmd_msg);
-
-		ros::spinOnce();
-		rate.sleep();
-	}
 
 private:
-	static double bound(double value, int min, int max)
-	{
-		if(value < min)
-			return min;
-		else if(value > max)
-			return max;
-		else
-			return value;
-	}
-
-private:
-	ros::NodeHandle& nh;
 	ros::Subscriber joy_sub;
-	ros::Publisher cmd_pub;
 	
 	double scale;
 	int resetButton;
 	int panAxis, tiltAxis;
-	int minPan, maxPan,	minTilt, maxTilt;
-
-	ros::Rate rate;
-
-	double pan;
-	double tilt;
-
 };
 
 int main(int argc, char *argv[])
