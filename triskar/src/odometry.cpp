4428cd8324e84b105c1133abe4cbd1b1277950a5
Alessandro Mazzoleni <alemazzo95@gmail.com>
2017-06-01 10:40:38 +0200

    Replaced publication on demo topic odom with sending transform to tf

--- triskar/src/odometry.cpp
+++ triskar/src/odometry.cpp
@@ -1,66 +1,50 @@
 #include <ros/ros.h>
-#include <geometry_msgs/TwistStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <math.h>
-
-#include <iostream>
-#include <string>
-#include <unistd.h>
-
-#include <sstream>
-#include <cstdio>
-
-
+#include <tf/transform_broadcaster.h>
 #include <boost/array.hpp>
-
 #include <boost/numeric/odeint.hpp>
+
 using namespace boost::numeric::odeint;
+using namespace tf;
 
-
-ros::Publisher odomPub;
-
-/*
- *  Differential Equations:
- *
- *  dx/dt  = cos(th) * vx - sin(th) * vy  = f(t, x)
- *  dy/dt  = sin(th) * vx + cos(th) * vy  = f(t, y)
- *  dth/dt = w                            = f(t, th)
- *
- */
+tf::TransformBroadcaster* odomBroadcaster;
 
 typedef boost::array< double , 3 > vector;
-
 vector odomPos = {0,0,0}, inputVel;
 
 void publishOdom() {
 
-    nav_msgs::Odometry odom;
-    odom.header.frame_id = "odom";
-    odom.child_frame_id = "base_link";
+    //create quaternion from theta
+    geometry_msgs::Quaternion odomQuat = tf::createQuaternionMsgFromYaw(odomPos[2]);
 
-    //set the position
-    odom.pose.pose.position.x = odomPos[0];
-    odom.pose.pose.position.y = odomPos[1];
-    odom.pose.pose.position.z = 0.0;
-    
-    //set the orientation
-    odom.pose.pose.orientation.x = 0;
-    odom.pose.pose.orientation.y = 0;
-    odom.pose.pose.orientation.z = odomPos[2];
-    odom.pose.pose.orientation.w = 0;
-    
-    //set the velocity
-    odom.twist.twist.linear.x = inputVel[0];
-    odom.twist.twist.linear.y = inputVel[1];
-    odom.twist.twist.angular.z = inputVel[2];
+    geometry_msgs::TransformStamped odomTrans;
+    odomTrans.header.stamp = ros::Time::now();
+    odomTrans.header.frame_id = "odom";
+    odomTrans.child_frame_id = "base_link";
 
-    odomPub.publish(odom);
+    odomTrans.transform.translation.x = odomPos[0];
+    odomTrans.transform.translation.y = odomPos[1];
+    odomTrans.transform.translation.z = 0.0;
+    odomTrans.transform.rotation = odomQuat;
+
+    //send the transform over tf
+    odomBroadcaster->sendTransform(odomTrans);
 }
 
-void f(vector &ds, double dt, const vector &s) {
-    ds[0] = (cos(s[2]) * inputVel[0] - sin(s[2]) * inputVel[1]);
-    ds[1] = (sin(s[2]) * inputVel[0] + cos(s[2]) * inputVel[1]);
-    ds[2] = inputVel[2];
+/*
+ *  Differential Equations:
+ *
+ *  dx/dt  = cos(th) * vx - sin(th) * vy
+ *  dy/dt  = sin(th) * vx + cos(th) * vy
+ *  dth/dt = w
+ *
+ */
+
+void f(vector &dsdt, double dt, const vector &s) {
+    dsdt[0] = (cos(s[2]) * inputVel[0] - sin(s[2]) * inputVel[1]);
+    dsdt[1] = (sin(s[2]) * inputVel[0] + cos(s[2]) * inputVel[1]);
+    dsdt[2] = inputVel[2];
 }
 
 void velCallback(const geometry_msgs::Twist::ConstPtr& msg) {
@@ -71,22 +55,22 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& msg) {
     double dt = 0.01; //0.01 s
     vector k1, k2, k3, k4;
     
-    //calcolo k1
+    //calculate k1
     f(k1, 0, odomPos);
     
-    //calcolo k2
+    //calculate k2
     vector sk2;
     for(int i=0; i<3; i++)
         sk2[i] = odomPos[i] + 1/2 * k1[i] * dt;
     f(k2, dt/2, sk2);
     
-    //calcolo k3
+    //calculate k3
     vector sk3;
     for(int i=0; i<3; i++)
         sk3[i] = odomPos[i] + 1/2 * k2[i] * dt;
     f(k3, dt/2, sk3);
     
-    //calcolo k4
+    //calculate k4
     vector sk4;
     for(int i=0; i<3; i++)
         sk4[i] = odomPos[i] + k3[i] * dt;
@@ -102,7 +86,7 @@ int main(int argc, char ** argv) {
     ros::init(argc, argv, "odometry");
     ros::NodeHandle n;
     ros::Subscriber velSub = n.subscribe("vel", 100, velCallback);
-    odomPub = n.advertise<nav_msgs::Odometry>("odom", 100);
+    odomBroadcaster = new tf::TransformBroadcaster();
 
     ros::spin();
 }
