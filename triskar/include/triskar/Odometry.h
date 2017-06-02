1499592c858bc5ff9874ba61c9013abe825f5538
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2017-06-02 14:58:23 +0200

    added Odometry.h

---
+++ triskar/include/triskar/Odometry.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_TRISKAR_ODOMETRY_H_
+#define INCLUDE_TRISKAR_ODOMETRY_H_
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Odometry {
+public:
+	Odometry();
+	virtual void integrate(vector<double> velocity, double dt) = 0;
+	void calculateVariation(vector<double> *variation, const vector<double>& position, const vector<double>& velocity);
+	vector<double> getPosition();
+
+protected:
+	vector <double>position;
+};
+
+#endif /* INCLUDE_TRISKAR_ODOMETRY_H_ */
