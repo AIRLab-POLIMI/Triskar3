7b3dc266e3143a2c532d56d9d40a2a8855d20d4a
Filip Krasniqi <filip.krasniqi@mail.polimi.it>
2017-06-02 15:04:24 +0200

    added EuleroOdometry.h

---
+++ triskar/include/triskar/EuleroOdometry.h
@@ -0,0 +1,19 @@
+#ifndef INCLUDE_TRISKAR_EULERO_ODOMETRY_H_
+#define INCLUDE_TRISKAR_EULERO_ODOMETRY_H_
+
+#include <vector>
+#include <math.h>
+#include <string>
+#include "triskar/Odometry.h"
+
+using namespace std;
+
+class EuleroOdometry : public Odometry{
+public:
+	using Odometry::Odometry;
+	void integrate(const vector<double>& velocity, double dt) override;
+	void calculateVariation(vector<double> *variation, const vector<double>& position, const vector<double>& velocity);
+	vector<double> getPosition();
+};
+
+#endif /* INCLUDE_TRISKAR_EULERO_ODOMETRY_H_ */
